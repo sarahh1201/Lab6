486a76b8fc9cc3efcaaed6e8a0420d947c0b0a84
Sarah <shy_h@telus.net>
2026-02-24 09:50:10 -0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/lab6/MainActivityTest2.java
@@ -0,0 +1,38 @@
+package com.example.lab6;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        ViewInteraction textView = onView(
+                allOf(withText("Hello World!"),
+                        withParent(allOf(withId(R.id.main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        textView.check(doesNotExist());
+    }
+}
